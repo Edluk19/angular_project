1ae1688d854aa84de1893ce152191e1be5302362
Eduard Lukashchuk <eduardluk37@gmail.com>
2024-06-18 21:20:13 +0300

    create

---
+++ src/app/components/primeng-movie-card/primeng-movie-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primeng-movie-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './primeng-movie-card.component.html',
+  styleUrl: './primeng-movie-card.component.scss'
+})
+export class PrimengMovieCardComponent {
+
+}
