ba6924fb1142da5c3986fb509b43ddb06586d7b2
Eduard Lukashchuk <eduardluk37@gmail.com>
2024-07-12 21:15:55 +0300

    create

--- src/app/pages/popular-page/popular-page.component.ts
+++ src/app/pages/popular-page/popular-page.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MovieListComponent } from '../../components/movie-list/movie-list.component';
 import { RouterLink, RouterModule } from '@angular/router';
 import { HeaderMovieComponent } from '../../components/header-movie/header-movie.component';
 import { movies } from '../../mock-data';
+import { MovieService } from '../../servises/movie/movie.service';
 
 @Component({
   selector: 'app-popular-page',
@@ -11,25 +12,27 @@ import { movies } from '../../mock-data';
   templateUrl: './popular-page.component.html',
   styleUrl: './popular-page.component.scss'
 })
-export class PopularPageComponent {
-  movies = movies;
-  favoriteMovieListIds: string[] = [];
-  watchLaterMovieListIds: string[] = [];
-
-
-
-  addToFavorites(id: any) {
-    if (!this.favoriteMovieListIds.includes(id)) {
-      this.favoriteMovieListIds.push(id);
-    }
-
-
+export class PopularPageComponent implements OnInit {
+  popularMovies: any[] = [];
+  // favoriteMovieListIds: string[] = [];
+  // watchLaterMovieListIds: string[] = [];
+  constructor(private movieService: MovieService) { }
+  ngOnInit() {
+    this.popularMovies = this.movieService.getPopularMovies();
   }
 
-  addToWatchlist(id: any) {
-    if (!this.watchLaterMovieListIds.includes(id)) {
-      this.watchLaterMovieListIds.push(id);
-    }
+  // addToFavorites(id: any) {
+  //   if (!this.favoriteMovieListIds.includes(id)) {
+  //     this.favoriteMovieListIds.push(id);
+  //   }
 
-  }
+
+  // }
+
+  // addToWatchlist(id: any) {
+  //   if (!this.watchLaterMovieListIds.includes(id)) {
+  //     this.watchLaterMovieListIds.push(id);
+  //   }
+
+  // }
 }
